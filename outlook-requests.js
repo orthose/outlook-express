f9fd511ed1c6694e96bc176c7a6e91fa42c9bd87
maxime vincent <maxime.vincent1@u-psud.fr>
2021-06-14 22:02:49 +0200

    Bugfix: Attente chargement fichier avant appel ajax d'envoi

--- outlook-requests.js
+++ outlook-requests.js
@@ -16,6 +16,7 @@ function get_requests(url_request, fdone) {
       "Authorization": "Bearer " + " " + token_mg
     }
   }).done(function(res) {
+    
     // Appel Ajax synchrone car liste chaînée de requêtes
     function get_events_nextLink(nextLink) {
       let res = {};
@@ -51,7 +52,7 @@ function get_requests(url_request, fdone) {
     // Appel de la callback
     fdone(filtered_res);
   }).fail(function(e) {
-    console.log(e);
+    //console.log(e);
     error();
   });
 }
@@ -93,6 +94,7 @@ function get_events(id_calendar) {
   //+"&$orderby=value/start/dateTime" // Ne fonctionne pas (Bad Request)
   //console.log(url);
   
+  // Affichage sous format français
   function simple_date(date) {
     return date.replace(/([0-9]+)-([0-9]+)-([0-9]+)T([0-9]+):([0-9]+).*/, "$3/$2/$1 $4:$5")
   }
@@ -114,14 +116,17 @@ function get_events(id_calendar) {
         +"<div class='select_file' hidden><label>Parcourir...<input type=file name='image'></label>"
         +"<input type='submit' value='Envoyer'><p>Aucun fichier sélectionné</p></div>"
         +"</td></tr>");
+      // Affichage du fichier sélectionné
       line.find(".select_file input[type=file]").on("change", function() {
         line.find(".select_file p")
           .css("color", "black")
           .html($(this)[0].files[0].name + " sélectionné");
       });
+      // Envoi du fichier
       line.find(".select_file input[type=submit]").on("click", function() {
-        put_file(line.find(".select_file input[type=file]"), line_json["id"]);
+        put_file(line.find(".select_file input[type=file]"), id_calendar, line_json["id"]);
       });
+      // Animation de déroulement de l'évènement
       line.find(".subject").on("click", function() {show_event(line_json["id"])});
       events.append(line);
     });
@@ -132,37 +137,44 @@ function get_events(id_calendar) {
 }
 
 // Envoi d'un fichier pièce-jointe à un évènement
-function put_file(tag, id_event) {
+function put_file(tag, id_calendar, id_event) {
+  
+  // Un fichier doit avoir été sélectionné
   if ($(tag)[0].files["length"] > 0) {
     const file_image = $(tag)[0].files[0];
     const reader = new FileReader();
     let b64_img = "";
+    // Fonctione exécutée à la fin du chargement du fichier
     reader.onloadend = function() {
+      // Conversion du fichier en base64
       b64_img = reader.result.replace(/^data:.+;base64,/, '');
-      //console.log(b64_img);
+      
+      // Appel Ajax après le chargement du fichier pour que b64_img != ""
+      $.ajax({
+        url: "https://graph.microsoft.com/v1.0/me/calendars/"+id_calendar
+        +"/events/"+id_event+"/attachments",
+        type: 'POST',
+        data: JSON.stringify({ 
+          "@odata.type": "#microsoft.graph.fileAttachment",
+          "name": file_image["name"],
+          "contentBytes": b64_img
+        }),
+        contentType: 'application/json',
+        dataType: 'json',
+        headers: {
+          "Authorization": "Bearer " + " " + token_mg
+        }
+      }).done(function(res) {
+        //console.log(res);
+        $("#"+escape_id(id_event)+" .select_file p")
+          .css("color", "green")
+          .html(file_image["name"] + " a bien été envoyé");
+      }).fail(function(e) {
+        //console.log(e);
+        error();
+      });
     };
+    // Chargement du fichier
     reader.readAsDataURL(file_image);
-    $.ajax({
-      url: "https://graph.microsoft.com/v1.0/me/events/"+id_event+"/attachments",
-      type: 'POST',
-      data: JSON.stringify({ 
-        "@odata.type": "#microsoft.graph.fileAttachment",
-        "name": file_image["name"],
-        "contentBytes": b64_img
-      }),
-      contentType: 'application/json',
-      dataType: 'json',
-      headers: {
-        "Authorization": "Bearer " + " " + token_mg
-      }
-    }).done(function(res) {
-      //console.log(res);
-      $("#"+escape_id(id_event)+" .select_file p")
-        .css("color", "green")
-        .html(file_image["name"] + " a bien été envoyé");
-    }).fail(function(e) {
-      //console.log(e);
-      error();
-    })
   }
 }
